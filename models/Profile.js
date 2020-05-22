89977d15e55827192b4b826f639468b4f391260d
standmarsh <agrawalshagun07@gmail.com>
2020-05-23 04:27:26 +0530

    just fixing a little thinf

---
+++ models/Profile.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  website: {
+    type: String,
+  },
+  steamusername: {
+    type: String,
+  },
+  xboxid: {
+    type: String,
+  },
+  playstationid: {
+    type: String,
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
