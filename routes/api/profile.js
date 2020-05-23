39066e8b09b501112f872671a9896e38198182fb
standmarsh <agrawalshagun07@gmail.com>
2020-05-23 07:29:48 +0530

    finished with profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -89,4 +89,60 @@ router.post(
   }
 );
 
+// @route     GET api/profile
+// @desc      Get All Profiles
+// @access    Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get Profile by User Id
+// @access    Public
+router.get(
+  "/user/:user_id",
+  checkObjectId("user_id"),
+  async ({ params: { user_id } }, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: user_id,
+      }).populate("user", ["name", "avatar"]);
+
+      if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
+
+// @route    DELETE api/profile
+// @desc     Delete Profile, User & Posts
+// @access   Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    //Remove User Posts
+    await Post.deleteMany({ user: req.user.id });
+
+    //Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    //Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
