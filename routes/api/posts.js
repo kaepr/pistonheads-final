5705866bbf2e0a2598f3be60e9eab9cb2f893202
standmarsh <agrawalshagun07@gmail.com>
2020-05-23 15:18:56 +0530

    more functioality in post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,64 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route GET api/posts
-// @desc Test Route
-// @access Public
+const Post = require("../../models/Post");
+const User = require("../../models/User");
+const checkObjectById = require("../../middleware/checkObjectId");
 
-router.get("/", (req, res) => res.send("Posts Route"));
+// @route       POST api/posts
+// @desc        Create a Post
+// @access      Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is Required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route       GET api/posts
+// @desc        Get All Posts
+// @access      Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+
+// @route       GET api/posts/:id
+// @desc        Get Post by ID
+// @access      Private
+
+
+
+
 
 module.exports = router;
