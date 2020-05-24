eb0c20720a2383f711360a9bf2b12794572540e1
standmarsh <agrawalshagun07@gmail.com>
2020-05-24 05:37:20 +0530

    final changes

---
+++ client/src/components/post/CommentForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addComment } from "../../actions/post";
+
+const CommentForm = ({ postId, addComment }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <div className="form">
+      <div className="bg-primary">
+        <h3>Leave a Comment</h3>
+      </div>
+      <form
+        className=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          addComment(postId, { text });
+          setText("");
+        }}
+      >
+        <textarea
+          name="text"
+          cols="30"
+          rows="5"
+          placeholder="Comment the post"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        />
+        <input type="submit" className="btn btn-dark" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addComment })(CommentForm);
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -8,6 +8,9 @@ import Dashboard from "../dashboard/Dashboard";
 import PrivateRoute from "./PrivateRoute";
 import ProfileForm from "../profile-forms/ProfileForm";
 import Profiles from "../profiles/Profiles";
+import Profile from "../profile/Profile";
+import Posts from "../posts/Posts";
+import Post from "../post/Post";
 
 const Routes = (props) => {
   return (
@@ -17,9 +20,12 @@ const Routes = (props) => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/profile/:id" component={Profile} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
+        <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
       </Switch>
     </section>
   );
