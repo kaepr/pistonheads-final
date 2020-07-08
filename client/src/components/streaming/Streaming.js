86ac91e4de8c707ce1a3124993d2f63a9f3e84bb
standmarsh <agrawalshagun07@gmail.com>
2020-07-08 16:47:47 +0530

    fixing the link

--- client/src/components/streaming/Streaming.js
+++ client/src/components/streaming/Streaming.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Streaming = props => {
-    return (
-        <div>
-            <a href="https://ec2-13-235-16-177.ap-south-1.compute.amazonaws.com:8443/#">redirects to the streaming section</a>
-        </div>
-    )
-}
+const Streaming = (props) => {
+  return (
+    <div>
+      <a href="https://ec2-52-66-246-133.ap-south-1.compute.amazonaws.com:8443/">
+        redirects to the streaming section
+      </a>
+    </div>
+  );
+};
 
-Streaming.propTypes = {
+Streaming.propTypes = {};
 
-}
-
-export default Streaming
+export default Streaming;
